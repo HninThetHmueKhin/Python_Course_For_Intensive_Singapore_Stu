7028dc93e368db1dc9f11143443200e683ef5df0
Khine <khinezar70122107@gmail.com>
2023-04-02 18:12:22 -1200

    standard output day2

---
+++ First_Month/2.standardOutput.py
@@ -0,0 +1,20 @@
+#format string
+name = "KK"
+subject = "Python"
+print(f'Hello,{name}.Subject:{subject}')
+
+#format
+age = 200000.998732
+print("The value of age: %.3f"%age)
+#%5d
+#%s
+#%.f
+
+print("Delta:%3d,encoding:%4.2f"%(11,99.888))
+#% parameter
+#2 length
+#d integer
+
+#f - floating
+
+print("Delta:{0:2d},encoding:{1:4.2f}".format(12,12.1212))
