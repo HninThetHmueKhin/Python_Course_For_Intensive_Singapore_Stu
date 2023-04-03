48af7636331f8d48144a5e9d15a14cc713cf84ba
Khine <khinezar70122107@gmail.com>
2023-04-02 18:29:17 -1200

    standard input

---
+++ First_Month/3.standardInput.py
@@ -0,0 +1,6 @@
+# firstNo = int(input("Please enter first Number : "))
+# secondNo = int(input("Please enter second Number : "))
+# result = firstNo+secondNo#34
+# print(f'Result:{result}')
+name = input("Please enter your name: ").strip()
+print("Hello: ",name)
