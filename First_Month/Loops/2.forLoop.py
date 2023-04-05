c84093636173f40f8d15b67847119149f3a5ee57
Khine <khinezar70122107@gmail.com>
2023-04-04 18:54:48 -1200

    loop lesson

---
+++ First_Month/Loops/2.forLoop.py
@@ -0,0 +1,17 @@
+# for(int i=0;i<5;i++){
+#     System.out.print(i)
+# }
+# #
+# print(range(5))
+
+# for i in range(2,10,2):#0 1 2 3 4#start,end,step value for range function#last ---> exclusive
+#     print(i)#2 4 6 8
+
+numbers = [1,2,3,4,5]
+name = "Khine"
+#for i in sequence list or string:
+# for i in name:
+#     print(i,end="")
+
+for j in numbers:
+    print(j)
