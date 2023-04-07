85499a19bfddf4264cfab81fe173cc38717377d9
Khine <khinezar70122107@gmail.com>
2023-04-06 17:36:38 -1200

    string lesson2

---
+++ First_Month/Step1/2.stringLesson2.py
@@ -0,0 +1,15 @@
+name,age = input("Please enter your name : ").split(" ")#unpacking
+#any whitespace from the beginning or the end
+# print(your_name.upper())
+# print(your_name.lower())
+print(name,":",age)
+
+
+#replace
+# animalName = "Tiger"
+# print(animalName.replace("T","t"))#old,new
+#
+# #split
+# #return list
+# text = "Hello.Python"
+# print(text.split("."))
