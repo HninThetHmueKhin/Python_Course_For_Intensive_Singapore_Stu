b0490f87080e2d73b98f46183090dada69427ffa
Khine <khinezar70122107@gmail.com>
2023-04-06 18:14:15 -1200

    string lesson2

---
+++ First_Month/Step1/3.stringLesson3.py
@@ -0,0 +1,12 @@
+# firstName = input("Enter your first name: ")
+# lastName = input("Enter your last name: ")
+#
+# print(firstName+" "+lastName)#concatenate strings
+
+
+#Escape Characters
+print('We are from Myanmar\'s country')
+print("Or operators \\ (backslash)")
+print("Khine\nZar")#new line
+
+print("Khine\tZar")
