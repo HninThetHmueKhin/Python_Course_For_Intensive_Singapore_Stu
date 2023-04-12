55447b17f94368b7932d6c4c5f330dc9283bd628
Khine <khinezar70122107@gmail.com>
2023-04-11 17:42:21 -1200

    list lesson2 remove

---
+++ First_Month/Step1/8.ListLesson2.py
@@ -0,0 +1,10 @@
+#pop
+amazon_cart = ['notebooks','sunglasses','toys']
+pop_element = amazon_cart.pop(1)#pop fun returns
+# print("After pop",amazon_cart)
+# print("Pop element: ",pop_element)
+
+#remove doesn't return removing element
+r_e = amazon_cart.remove("toys")
+print(amazon_cart)
+print(r_e)#None
