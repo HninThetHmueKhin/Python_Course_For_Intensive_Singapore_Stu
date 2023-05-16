e5db37e74532d8b8d2671b1dcec47818fcaa9d45
Khine <khinezar70122107@gmail.com>
2023-05-15 18:07:11 -1200

    may 15

---
+++ Second_Month/1.osSample.py
@@ -0,0 +1,12 @@
+import os
+
+def current_path():
+    print("Current Working Directory Before: ")
+    print(os.getcwd())
+
+if __name__ == '__main__':
+    current_path()
+
+    os.chdir("../")
+
+    current_path()
