b81a4cf312f9dd428794768ff50e46f73be55a21
Khine <khinezar70122107@gmail.com>
2023-06-19 19:16:00 -1200

    access modifier testing

---
+++ Second_Month/OOP/accessModifier/App.py
@@ -0,0 +1,13 @@
+from Machine import *
+
+if __name__ == '__main__':
+    machine = Machine()
+    print(machine.id)
+    print(machine.count)
+    print(machine._color)
+    print(machine._Machine__name)
+
+
+"""
+_ClassName__privateVarName
+"""
