ba6ecc22da1f51b98501b33953002cbb669f6fba
Khine <khinezar70122107@gmail.com>
2023-06-20 20:00:27 -1200

    json lesson jun 20

--- Second_Month/JsonLessons/3.jsonSerialization2.py
+++ Second_Month/JsonLessons/3.jsonSerialization2.py
@@ -1,9 +1,9 @@
 import json
-myJson = {
+myPython = {
   "name" : "khine",
   "age" : 25,
   "hobby" : ["coding","training","building IOT"]
 }
 
 with open("jdataFile.json","w") as jsFile:
-    json.dump(myJson,jsFile,indent=2,sort_keys=True)
+    json.dump(myPython,jsFile,indent=2,sort_keys=True)
